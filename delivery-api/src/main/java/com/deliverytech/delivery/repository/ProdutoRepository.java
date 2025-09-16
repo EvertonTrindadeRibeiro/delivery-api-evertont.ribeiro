7d40bc0c205b6d76736dd9045cf4f4f02e3dbd27
Everton Trindade Ribeiro <trindade.everton@aluno.ifsp.edu.br>
2025-09-15 22:42:19 -0300

    Corrige estrutura de pacotes, repositórios e configurações do projeto

--- delivery-api/src/main/java/com/deliverytech/delivery/repository/ProdutoRepository.java
+++ delivery-api/src/main/java/com/deliverytech/delivery/repository/ProdutoRepository.java
@@ -2,7 +2,6 @@ package com.deliverytech.delivery.repository
 
 import com.deliverytech.delivery.entity.Produto;
 import org.springframework.data.jpa.repository.JpaRepository; 
-import org.springframework.stereotype.Repository; 
 import java.math.BigDecimal; 
 import java.util.List;
 
