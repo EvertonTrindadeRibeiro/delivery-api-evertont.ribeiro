ebc062cea0e107a6c6e1291162978930d949f386
Everton T. Ribeiro <trindade.everton@aluno.ifsp.edu.br>
2025-09-23 20:59:37 -0300

    Ajuste na classe PedidoService

--- delivery-api/src/main/java/com/deliverytech/delivery/service/PedidoService.java
+++ delivery-api/src/main/java/com/deliverytech/delivery/service/PedidoService.java
@@ -3,17 +3,23 @@ package com.deliverytech.delivery.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.deliverytech.delivery.entity.Pedido;
+import com.deliverytech.delivery.entity.ItemPedido;
+import com.deliverytech.delivery.entity.Produto;
 import com.deliverytech.delivery.repository.PedidoRepository;
+import com.deliverytech.delivery.repository.ProdutoRepository;
 
 @Service
 public class PedidoService {
 
     private final PedidoRepository pedidoRepository;
+    private final ProdutoRepository produtoRepository;
 
-    public PedidoService(PedidoRepository pedidoRepository) {
+    public PedidoService(PedidoRepository pedidoRepository, ProdutoRepository produtoRepository) {
         this.pedidoRepository = pedidoRepository;
+        this.produtoRepository = produtoRepository;
     }
 
     public List<Pedido> listarTodos() {
@@ -38,5 +44,18 @@ public class PedidoService {
 
     public List<Pedido> listarPorRestaurante(Long restauranteId) {
         return pedidoRepository.findByRestauranteId(restauranteId);
+    }
+
+    @Transactional
+    public Pedido salvarPedidoComItens(Pedido pedido) {
+        pedidoRepository.save(pedido);
+
+        for (ItemPedido item : pedido.getItens()) {
+            Produto produto = item.getProduto();
+            produto.setEstoque(produto.getEstoque() - item.getQuantidade());
+            produtoRepository.save(produto);
+        }
+
+        return pedido;
     }
 }
